49a6366355b81dd1ff7af02f05e9e65463ef6a90
Clara Pagotto <clarapagotto@gmail.com>
2020-08-02 19:01:37 -0300

    update para heroku

--- reactflix/src/pages/cadastro/Categoria/index.js
+++ reactflix/src/pages/cadastro/Categoria/index.js
@@ -25,7 +25,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost') 
+    ? 'http://localhost:8080/categorias'
+    : 'http://localhost:8080/categorias';
     fetch(URL).then(async (serverAnswer) => {
       const answer = await serverAnswer.json();
       setCategorias([
